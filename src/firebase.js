cbcadb07f6e8551aa6a3d83631863ea254afa817
Ywette <ywette.pro@gmail.com>
2021-05-23 21:52:00 +0300

    airbnb-clone-react
    airbnb-clone-react

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBrth99oNj4BIg7oExownUdeW83cW_vllQ",
+    authDomain: "airbnb-second.firebaseapp.com",
+    projectId: "airbnb-second",
+    storageBucket: "airbnb-second.appspot.com",
+    messagingSenderId: "216373051867",
+    appId: "1:216373051867:web:67f2928b0062f685f3a7fe",
+    measurementId: "G-PM6BDFP297"
+  };
